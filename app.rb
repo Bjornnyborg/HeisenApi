8bdb9276e603d60df053485722f185e9a7c15bc1
Bjornnyborg <Bjornnyborg@hotmail.com>
2020-06-24 00:58:54 +0200

    init app

---
+++ app.rb
@@ -0,0 +1,48 @@
+# app.rb
+require 'sinatra'
+require 'sinatra/cross_origin'
+require 'json'
+require 'mysql2'
+
+
+class App < Sinatra::Base
+
+  configure do
+    enable :cross_origin
+  end
+
+  before do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
+
+  client = Mysql2::Client.new(:host => "localhost", :username => "root", :database => "heisenbase")
+  client.query("CREATE TABLE IF NOT EXISTS \
+    highscores(Id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(25), time INT, level INT, createdDate datetime DEFAULT(CURTIME()))")
+
+  get '/highscore/:level' do
+    scores = []
+
+    client.query("SELECT id, name, time, level, createdDate FROM highscores WHERE level='#{params['level']}' ORDER BY time ASC;").each do |row|
+      scores.push(row)
+    end
+
+    content_type :json
+    scores.to_json
+
+  end
+
+  post '/highscore' do
+    push = JSON.parse(request.body.read)
+
+    client.query("INSERT INTO highscores (name, time, level)
+      VALUES ('#{push["name"]}', '#{push["time"]}', '#{push["level"]}')")
+
+  end
+
+  options "*" do
+    response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    200
+  end
+end
