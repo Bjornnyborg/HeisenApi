3144227068bd351c7a9ba5e1ad68779361ce1c81
Bjornnyborg <Bjornnyborg@hotmail.com>
2020-06-24 01:47:37 +0200

    Updated config load

--- app.rb
+++ app.rb
@@ -16,7 +16,8 @@ class App < Sinatra::Base
     response.headers['Access-Control-Allow-Origin'] = '*'
   end
 
-  config = YAML::load_file("config/database.yml")["production"]
+  config = YAML.load(ERB.new(File.read(File.join("config","database.yml"))).result)["production"]
+
   client = Mysql2::Client.new(:host => config['hostname'], :username => config['username'], :password => config['password'], :database => config['database'], :socket => config['socket'])
   client.query("CREATE TABLE IF NOT EXISTS \
     highscores(Id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(25), time INT, level INT, createdDate TIMESTAMP NOT NULL DEFAULT current_timestamp)")
